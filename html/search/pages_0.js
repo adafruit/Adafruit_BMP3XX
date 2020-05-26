218b2010f40f14f33199d01966945374ddc3ca8b
Doxygen CI <travis@travis-ci.org>
2020-05-26 17:26:45 +0000

    Deploy code docs to GitHub Pages Travis build: 24
    Commit: 7fd864818cbfde7985405ea1aecc56b2b9eb0eb0

--- html/search/pages_0.js
+++ html/search/pages_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['adafruit_20bmp3xx_20temperature_20_26_20barometric_20pressure_20sensor_20driver',['Adafruit BMP3XX temperature &amp; barometric pressure sensor driver',['../index.html',1,'']]],
-  ['adafruit_5fbmp3xx_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2fgithub_2ecom_2fadafruit_2fadafruit_5fbmp3xx_2fworkflows_2farduino_2520library_2520ci_2fbadge_2esvg_29_5d_28https_3a_2f_2fgithub_2ecom_2fadafruit_2fadafruit_5fbmp3xx_2factions_29',['Adafruit_BMP3XX [![Build Status](https://github.com/adafruit/Adafruit_BMP3XX/workflows/Arduino%20Library%20CI/badge.svg)](https://github.com/adafruit/Adafruit_BMP3XX/actions)',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['adafruit_5fbmp3xx_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2ecom_2fadafruit_2fadafruit_5fbmp3xx_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2ecom_2fadafruit_2fadafruit_5fbmp3xx_29',['Adafruit_BMP3XX [![Build Status](https://travis-ci.com/adafruit/Adafruit_BMP3XX.svg?branch=master)](https://travis-ci.com/adafruit/Adafruit_BMP3XX)',['../md__r_e_a_d_m_e.html',1,'']]]
 ];
