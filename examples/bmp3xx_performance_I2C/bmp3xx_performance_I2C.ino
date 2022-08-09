f296aaabdc0784b8cc5ea1e5c59d620c9490b4c0
rob tillaart <rob.tillaart@gmail.com>
2022-08-09 19:05:43 +0200

    add example

---
+++ examples/bmp3xx_performance_I2C/bmp3xx_performance_I2C.ino
@@ -0,0 +1,99 @@
+/***************************************************************************
+  This is a library for the BMP3XX temperature & pressure sensor
+
+  Designed specifically to work with the Adafruit BMP388 Breakout
+  ----> http://www.adafruit.com/products/3966
+
+  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
+  to interface.
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Rob TIllaart based upon work written by
+  Limor Fried & Kevin Townsend for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ***************************************************************************/
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP3XX.h"
+
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11
+#define BMP_CS 10
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BMP3XX bmp;
+
+uint32_t start, stop;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("Adafruit BMP388 / BMP390 performance measurement");
+
+  if (!bmp.begin_I2C()) {   // hardware I2C mode, can pass in address & alt Wire
+    //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode
+    //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
+    Serial.println("Could not find a valid BMP3 sensor, check wiring!");
+    while (1);
+  }
+
+  // Set up oversampling and filter initialization
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+
+  for (uint32_t speed = 50000; speed < 650000; speed += 50000)
+  {
+    Serial.print("I2C: \t");
+    Serial.println(speed);
+    Wire.setClock(speed);
+    measure();
+  }
+
+  Serial.println("\ndone...");
+}
+
+void loop() {
+}
+
+
+void measure() {
+  Serial.println();
+  delay(100);
+  start = micros();
+  bmp.performReading();
+  stop = micros();
+  Serial.print("performReading:\t");
+  Serial.println(stop - start);
+  delay(100);
+
+  start = micros();
+  bmp.readTemperature();
+  stop = micros();
+  Serial.print("readTemperature:");
+  Serial.println(stop - start);
+  delay(100);
+
+  start = micros();
+  bmp.readPressure();
+  stop = micros();
+  Serial.print("readPressure:\t");
+  Serial.println(stop - start);
+  delay(100);
+
+  start = micros();
+  bmp.readAltitude(SEALEVELPRESSURE_HPA);
+  stop = micros();
+  Serial.print("readAltitude:\t");
+  Serial.println(stop - start);
+  delay(100);
+  Serial.println();
+}
