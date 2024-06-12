fc76d15c3f41ae0549895b43e16851d86e06b442
Doxygen CI <ci-arduino@invalid>
2024-06-12 21:34:08 +0000

    Deploy docs to GitHub Pages from commit 33b19ccb6d
    Commit: 33b19ccb6d11aeda4e433d3df5ed40a75bb0ee4a
    GitHub Actions run: 9490011591

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['begin_5fi2c',['begin_I2C',['../class_adafruit___b_m_p3_x_x.html#aeb1ce5bf3ef63601de4cb09ca0fd7034',1,'Adafruit_BMP3XX']]],
-  ['begin_5fspi',['begin_SPI',['../class_adafruit___b_m_p3_x_x.html#abd8e357b87ab768c538e7156fd36136a',1,'Adafruit_BMP3XX::begin_SPI(uint8_t cs_pin, SPIClass *theSPI=&amp;SPI)'],['../class_adafruit___b_m_p3_x_x.html#a7b0d11d50c71e0f65ef45cedfdc783e9',1,'Adafruit_BMP3XX::begin_SPI(int8_t cs_pin, int8_t sck_pin, int8_t miso_pin, int8_t mosi_pin)']]],
+  ['begin_5fspi',['begin_SPI',['../class_adafruit___b_m_p3_x_x.html#a26ba0d9838c5243b9ea7a11dd5afac9d',1,'Adafruit_BMP3XX::begin_SPI(uint8_t cs_pin, SPIClass *theSPI=&amp;SPI, uint32_t frequency=BMP3XX_DEFAULT_SPIFREQ)'],['../class_adafruit___b_m_p3_x_x.html#a2e8192a737c7a72f4597c433addf9ae1',1,'Adafruit_BMP3XX::begin_SPI(int8_t cs_pin, int8_t sck_pin, int8_t miso_pin, int8_t mosi_pin, uint32_t frequency=BMP3XX_DEFAULT_SPIFREQ)']]],
   ['bmp3xx_5fdefault_5faddress',['BMP3XX_DEFAULT_ADDRESS',['../_adafruit___b_m_p3_x_x_8h.html#aa075df147fcdbb640ffc929d03834d55',1,'Adafruit_BMP3XX.h']]],
   ['bmp3xx_5fdefault_5fspifreq',['BMP3XX_DEFAULT_SPIFREQ',['../_adafruit___b_m_p3_x_x_8h.html#a62d27a4042a0b28b262306cb8970290d',1,'Adafruit_BMP3XX.h']]]
 ];
--- html/search/functions_1.js
+++ html/search/functions_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['begin_5fi2c',['begin_I2C',['../class_adafruit___b_m_p3_x_x.html#aeb1ce5bf3ef63601de4cb09ca0fd7034',1,'Adafruit_BMP3XX']]],
-  ['begin_5fspi',['begin_SPI',['../class_adafruit___b_m_p3_x_x.html#abd8e357b87ab768c538e7156fd36136a',1,'Adafruit_BMP3XX::begin_SPI(uint8_t cs_pin, SPIClass *theSPI=&amp;SPI)'],['../class_adafruit___b_m_p3_x_x.html#a7b0d11d50c71e0f65ef45cedfdc783e9',1,'Adafruit_BMP3XX::begin_SPI(int8_t cs_pin, int8_t sck_pin, int8_t miso_pin, int8_t mosi_pin)']]]
+  ['begin_5fspi',['begin_SPI',['../class_adafruit___b_m_p3_x_x.html#a26ba0d9838c5243b9ea7a11dd5afac9d',1,'Adafruit_BMP3XX::begin_SPI(uint8_t cs_pin, SPIClass *theSPI=&amp;SPI, uint32_t frequency=BMP3XX_DEFAULT_SPIFREQ)'],['../class_adafruit___b_m_p3_x_x.html#a2e8192a737c7a72f4597c433addf9ae1',1,'Adafruit_BMP3XX::begin_SPI(int8_t cs_pin, int8_t sck_pin, int8_t miso_pin, int8_t mosi_pin, uint32_t frequency=BMP3XX_DEFAULT_SPIFREQ)']]]
 ];
